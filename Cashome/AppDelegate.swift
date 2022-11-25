56002217cf7d96bc82d9d82bbd53518354fd0aac
AIWASS23 <marcelodearaujo677@gmail.com>
2022-11-25 09:22:56 -0300

    VSCode

--- Cashome/AppDelegate.swift
+++ Cashome/AppDelegate.swift
@@ -12,9 +12,14 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    var window: UIWindow?
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [
+            UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+                window = UIWindow()
+                window?.rootViewController = ViewController()
+                window?.makeKeyAndVisible()
         return true
     }
 
