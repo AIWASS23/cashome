8429c2ad7feeb142436006c9df8acca4c1b5994d
AIWASS23 <marcelodearaujo677@gmail.com>
2022-11-30 09:43:52 -0300

    🧱 Label despesas

--- Cashome/Views/HomeScreen.swift
+++ Cashome/Views/HomeScreen.swift
@@ -9,18 +9,59 @@ import UIKit
 
 class HomeScreen: UIView {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    let titleExpense: UILabel = {
+        let label = UILabel()
+        label.text = "Despesas"
+        label.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        /* fazer uma extension func para reaproveitamento de Codigo */
+
+        return label
+    }()
+
+
+
     override init(frame: CGRect) {
-            super.init(frame: frame)
-            // linearGradientColorBackground()
-        }
+        super.init(frame: frame)
+        self.addSubview(titleExpense)
+        setupConstraints()
 
-        override func didMoveToWindow() {
-            super.didMoveToWindow()
-            linearGradientColorBackground()
-        }
+    }
 
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        linearGradientColorBackground()
 
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+
+            titleExpense.centerXAnchor.constraint(equalTo: centerXAnchor, constant: -110),
+            titleExpense.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -75),
+            titleExpense.widthAnchor.constraint(equalToConstant: 150),
+            titleExpense.heightAnchor.constraint(equalToConstant: 150)
+
+        ])
+    }
 }
